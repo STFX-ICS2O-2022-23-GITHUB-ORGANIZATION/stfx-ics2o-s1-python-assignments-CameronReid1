b58f078cd99fc939c8b8fcb9d909c50dd9610c26
s479343 <s479343@FXHC140M05.ocsb.local>
2022-11-25 12:50:33 -0500

    submitted answer

--- Unit 4/Q1Volume.py
+++ Unit 4/Q1Volume.py
@@ -1,1 +1,9 @@
-print("Hello World")
+print("What's the lenght")
+length = input()
+print("Whats the width:")
+width = input()
+print("whats the height")
+height = input()
+
+volume = int(length) * int(width) * int(height)
+print ( "Volume of a rectangular prism with dimensions of " + str(length) + ", " + str(width) + ", and " + str(height) + " is " + str(volume) + "." )
