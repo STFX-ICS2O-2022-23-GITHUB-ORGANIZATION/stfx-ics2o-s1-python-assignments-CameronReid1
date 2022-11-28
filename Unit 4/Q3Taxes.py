520b8fa0ea90ecb2d23344e2f78ff4a1186ca63c
s479343 <s479343@FXHC140M05.ocsb.local>
2022-11-28 12:24:17 -0500

    Taxes

---
+++ Unit 4/Q3Taxes.py
@@ -0,0 +1,48 @@
+var1 = 0
+var2 = 0
+var3 = 0
+var4 = 0
+var5 = 0
+var6 = 0
+var7 = 0
+var8 = 0
+var9 = 0
+var10 = 0
+cost = 0
+
+print("enter first number")
+var1 = int(input())
+
+print("enter Second number")
+var2 = int(input())
+
+print("enter Third number")
+var3 = int(input())
+
+print("enter Fourth number")
+var4 = int(input())
+
+print("enter Fifth number")
+var5 = int(input())
+
+print("enter Sixth number")
+var6 = int(input())
+
+print("enter Seven number")
+var7 = int(input())
+
+print("enter Eight number")
+var8 = int(input())
+
+print("enter Nine number")
+var9 = int(input())
+
+print("enter Ten number")
+var10 = int(input())
+
+print("The total cost without taxes is")
+cost = var1 + var2 + var3 + var4 + var5 + var6 + var7 + var8 + var8 + var9 + var10
+print(cost)
+
+print("The total cost with taxes is: ")
+print(cost * 1.13)
