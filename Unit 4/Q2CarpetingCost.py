671d3421e1c3e865f92a24238116757b4a494087
s479343 <s479343@FXHC140M05.ocsb.local>
2022-11-29 12:07:47 -0500

    Carpeting

---
+++ Unit 4/Q2CarpetingCost.py
@@ -0,0 +1,10 @@
+length = 0
+width = 0
+cost = 0
+
+length = int(input("What is the length: "))
+width = int(input("What is the width: "))
+
+cost = (int(length) * int(width) * int(2.25))
+
+print("The cost of carpeting per square metere is " + str(length) + "," + str(width) + "is" + str(cost) + ", ")
