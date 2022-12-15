b2ef80354f43de636dde0c369f22f0203b383c89
s479343 <s479343@FXHC140M05.ocsb.local>
2022-12-15 12:15:16 -0500

    Q2Average

---
+++ Unit 4/Q2Average.py
@@ -0,0 +1,25 @@
+# NAME OF AUTHOR:  Cameron ried
+# NAME OF THE PROGRAM:  Q2Average
+# DATE OF CREATION:  December 15th 2022
+# PURPOSE OF PROGRAM:  To print non zero numbers. Stops when zero is printed
+
+# VARIABLE DEFINITION
+
+# INPUT
+userInput = -1
+total = 0
+# PROCESSING
+while(userInput != 0):
+    print("Enter Number")
+    userInput = int(input())
+    total = total + userInput
+# OUTPUT
+print(total)
+
+
+
+
+
+
+
+    
