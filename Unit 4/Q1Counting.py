7fbf8fa053d032342ecd624190f4d275ebd9aa63
s479343 <s479343@FXHC140M05.ocsb.local>
2022-12-15 11:59:05 -0500

    Q1Counting

---
+++ Unit 4/Q1Counting.py
@@ -0,0 +1,23 @@
+# NAME OF AUTHOR:  Cameron Reid
+# NAME OF THE PROGRAM:  Q1Counting
+# DATE OF CREATION:  15th December 2022
+# PURPOSE OF PROGRAM:  To count by a certein number 
+
+# VARIABLE DEFINITION
+from ast import Num
+# INPUT
+num1 = 0
+num2 = 0
+# PROCESSING
+num1 = int(input("Enter one large number:"))
+num2 = int(input("Enter the number you want to count by"))
+for num1 in range (num1, 0, -num2):
+# OUTPUT
+ print(num1)
+
+
+
+
+
+
+
