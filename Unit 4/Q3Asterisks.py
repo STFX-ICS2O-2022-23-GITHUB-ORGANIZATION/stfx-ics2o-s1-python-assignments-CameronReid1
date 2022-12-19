d50500a4014eca49b3b4e77ee99b05ee7f3f7b70
s479343 <s479343@FXHC140M05.ocsb.local>
2022-12-19 11:29:32 -0500

    Q3Asterisks

---
+++ Unit 4/Q3Asterisks.py
@@ -0,0 +1,25 @@
+# NAME OF AUTHOR:  Cameron Reid
+# NAME OF THE PROGRAM:  Q3
+# DATE OF CREATION:  December 15th
+# PURPOSE OF PROGRAM:  to print astriks
+
+# VARIABLE DEFINITION
+import time
+
+# INPUT
+ROWS = 25
+CHARS = 80
+# PROCESSING
+for i in range (0, ROWS):
+
+    for j in range (0, CHARS):
+         print("*", end = "")
+         time.sleep(0.5)
+# OUTPUT
+    print("")
+
+
+
+
+
+
